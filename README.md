65fedc13b365a4aac5ea4bcc77bd8355e965030e
Gukjin Kim <exodus0315@outlook.com>
2022-02-23 23:10:43 +0900

    develop project 20220223

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gk-times
- Gukjin News
+
+Gukjin News
+
+<br/>
+
+### 20220223
+
+GK Times Project를 본격적으로 시작...
+
+기초적인 레이아웃 구성
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const getLatestNews = () => {
+let news = [];
+
+const getLatestNews = async () => {
   let url = new URL(
     "https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10"
   );
@@ -6,8 +8,10 @@ const getLatestNews = () => {
     "x-api-key": "-94HDVO4ARMW9cPwgcC1j_wKKD3thbjRW0jP9cJkikY",
   });
 
-  let response = fetch(url, { headers: header });
-  console.log(url);
+  let response = await fetch(url, { headers: header }); // ajax, http, fetch 등 다양한 방식 있음
+  let data = await response.json();
+  news = data.articles;
+  console.log(news);
 };
 
 getLatestNews();
