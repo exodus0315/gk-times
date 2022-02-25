586fb88f9690803fb5c2220b8520eae9bec148bf
Gukjin Kim <exodus0315@outlook.com>
2022-02-25 22:54:45 +0900

    develop 20220225
    Read readme.md

--- main.js
+++ main.js
@@ -6,50 +6,53 @@ menus.forEach((menu) =>
 
 let searchButton = document.getElementById("search-button");
 
+let url;
+
+const getNews = async () => {
+  try {
+    let header = new Headers({
+      "x-api-key": "-94HDVO4ARMW9cPwgcC1j_wKKD3thbjRW0jP9cJkikY",
+    });
+
+    let response = await fetch(url, { headers: header }); // ajax, http, fetch 등 다양한 방식 있음
+    let data = await response.json();
+    if (response.status == 200) {
+      if (data.total_hits == 0) {
+        throw new Error("검색된 결과값이 없습니다.");
+      }
+      news = data.articles;
+      console.log(news);
+      render();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.log("잡힌 에러는", error.message);
+    errorRender(error.message);
+  }
+};
+
 const getLatestNews = async () => {
-  let url = new URL(
+  url = new URL(
     "https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10"
   );
-  let header = new Headers({
-    "x-api-key": "-94HDVO4ARMW9cPwgcC1j_wKKD3thbjRW0jP9cJkikY",
-  });
-
-  let response = await fetch(url, { headers: header }); // ajax, http, fetch 등 다양한 방식 있음
-  let data = await response.json();
-  news = data.articles;
-  console.log(news);
-
-  render();
+  getNews();
 };
 
 const getNewsByTopic = async (event) => {
   let topic = event.target.textContent.toLowerCase();
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`
   );
-  let header = new Headers({
-    "x-api-key": "-94HDVO4ARMW9cPwgcC1j_wKKD3thbjRW0jP9cJkikY",
-  });
-  let response = await fetch(url, { headers: header });
-  let data = await response.json();
-  news = data.articles;
-
-  render();
+  getNews();
 };
 
 const getNewsByKeyword = async () => {
   let keyword = document.getElementById("search-input").value;
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`
   );
-  let header = new Headers({
-    "x-api-key": "-94HDVO4ARMW9cPwgcC1j_wKKD3thbjRW0jP9cJkikY",
-  });
-  let response = await fetch(url, { headers: header });
-  let data = await response.json();
-  news = data.articles;
-
-  render();
+  getNews();
 };
 
 const render = () => {
@@ -70,6 +73,13 @@ const render = () => {
     .join("");
 
   document.getElementById("news-board").innerHTML = newsHTML;
+};
+
+const errorRender = (message) => {
+  let errorHTML = `<div class="alert alert-danger text-center" role="alert">
+  ${message}
+</div>`;
+  document.getElementById("news-board").innerHTML = errorHTML;
 };
 
 searchButton.addEventListener("click", getNewsByKeyword); // 화살표 함수를 사용할 경우 일반 함수와 다르기 때문에 호이스팅이 안됨. 그래서 순서가 중요하기 때문에 addEventListener를 맨 밑으로 보냄(함수 정의보다 아래로, 함수 선언 -> 이벤트)
